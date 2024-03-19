fde257567033c44bf71bde9350f1da2b9400995f
Đào Duy An <daoduyan8801@gmail.com>
2024-03-19 09:30:04 +0700

    feat: add searchpage UI

---
+++ FE/SocialNetwork.Web_User/ClientApp/src/components/UsersSearch/UsersSearch.js
@@ -0,0 +1,48 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+import styles from './UsersSearch.module.scss';
+
+const cx = classNames.bind(styles);
+
+// function UsersSearch({ user }) {
+function UsersSearch() {
+    return (
+        // <div className={cx('user-container')}>
+        //     <Link to={`/user/${user.userName}`} className={cx('card-user')}>
+        //         <img
+        //             src={
+        //                 user.avatar
+        //                     ? user.avatar
+        //                     : 'https://www.gravatar.com/avatar/262cfa0997548c39953a9607a56f27da?d=wavatar&f=y'
+        //             }
+        //             alt="ava_img"
+        //             className={cx('post-avt')}
+        //         />
+
+        //         <div className={cx('user-info')}>
+        //             <Link to={`/user/${user.userName}`} className={cx('post-username')}>
+        //                 <span>{user.displayName ? user.displayName : user.userName}</span>
+        //             </Link>
+        //         </div>
+        //     </Link>
+        // </div>
+   
+            <Link to={`/user/`} className={cx('card-user')}>
+                <img
+                    src={'https://www.gravatar.com/avatar/262cfa0997548c39953a9607a56f27da?d=wavatar&f=y'}
+                    alt="ava_img"
+                    className={cx('post-avt')}
+                />
+
+                <div className={cx('user-info')}>
+                    <Link to={`/user/`} className={cx('post-username')}>
+                        <span>daoduyan</span>
+                    </Link>
+                </div>
+            </Link>
+     
+    );
+}
+
+export default UsersSearch;
--- FE/SocialNetwork.Web_User/ClientApp/src/pages/Search/Search.js
+++ FE/SocialNetwork.Web_User/ClientApp/src/pages/Search/Search.js
@@ -1,9 +1,104 @@
+import { useState } from 'react';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faFile } from '@fortawesome/free-solid-svg-icons';
 
+import PostItem from '~/components/PostItem';
+import UsersSearch from '~/components/UsersSearch';
+import Pagination from '@mui/material/Pagination';
+
+import styles from './Search.module.scss';
+import { styled } from '@mui/system';
+
+const StyledPagination = styled(Pagination)({
+    '& .MuiPaginationItem-root': {
+        fontSize: '1.5rem', 
+    },
+});
+
+const cx = classNames.bind(styles);
 
 function Search() {
-    return ( 
-        <h2>Search page</h2>
-     );
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const [posts, setPosts] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    const query = searchParams.get('q');
+    const type = searchParams.get('type');
+
+    return (
+        <div className={cx('search')}>
+            <div className={cx('search__container')}>
+                <div className={cx('search__heading')}>
+                    <h3 className={cx('search__heading-name')}>
+                        Kết quả tìm kiếm:
+                        <span> "{query}"</span>
+                    </h3>
+                </div>
+                <div className={cx('search__content')}>
+                    <div className={cx('search__content-body')}>
+                        <div className={cx('search__content-nav')}>
+                            <ul className={cx('search__content-nav-menu')}>
+                                <li
+                                    className={cx('search__content-nav-item')}
+                                    onClick={() => navigate(`/search?q=${query}&type=post`)}
+                                >
+                                    <div
+                                        className={cx('search__content-nav-item-wrapper', { active: type === 'post' })}
+                                    >
+                                        <Link to="/" className={cx('search__content-nav-link')}>
+                                            <FontAwesomeIcon icon={faFile} className={cx('search__content-nav-icon')} />
+                                            <span className={cx('search__content-nav-text')}>Bài viết</span>
+                                        </Link>
+                                    </div>
+                                </li>
+                                <li
+                                    className={cx('search__content-nav-item')}
+                                    onClick={() => navigate(`/search?q=${query}&type=user`)}
+                                >
+                                    <div
+                                        className={cx('search__content-nav-item-wrapper', { active: type === 'user' })}
+                                    >
+                                        <Link to="/" className={cx('search__content-nav-link')}>
+                                            <FontAwesomeIcon className={cx('search__content-nav-icon')} icon={faUser} />
+                                            <span className={cx('search__content-nav-text')}>Người dùng</span>
+                                        </Link>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                        {/* Search result */}
+                        <div className={cx('search__content-result')}>
+                            {type === 'post' ? (
+                                <>
+                                    {/* posts.map((post) => <PostItem post={post} key={post._id} /> */}
+                                    <PostItem key={1} />
+                                    <PostItem key={2} />
+                                </>
+                            ) : (
+                                <div className={cx('user-container')}>
+                                    {/* posts.map((post) => <PostItem post={post} key={post._id} /> */}
+                                    <UsersSearch key={1} />
+                                    <UsersSearch key={2} />
+                                    <UsersSearch key={3} />
+                                    <UsersSearch key={4} />
+                                    <UsersSearch key={5} />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('flex', 'justify-center', 'm-[30px]')}>
+                    <StyledPagination count={10} variant="outlined" color="primary" size="large" boundaryCount={2} showFirstButton showLastButton/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Search;
